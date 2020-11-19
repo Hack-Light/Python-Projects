45b3ed096d0c37a360ce10e94fc2fbd544ff716a
Hack-Light <onohsomtochukwubasil@gmail.com>
2020-11-19 02:35:41 +0100

    Finally

---
+++ Classes_Object/student.py
@@ -0,0 +1,6 @@
+class student:
+    def __init__(self,name,department,gpa,regular):
+        self.name = name
+        self.department = department
+        self.gpa = gpa
+        self.regular = regular
